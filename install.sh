e606a3abedb3d78b46c1f39ca171b69d9f9b7ad3
Ihsan Gunay <ihsan@firasetpartnerships.com>
2021-06-24 14:51:19 -0400

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+
+#Use pam_env.conf to set environment variables at startup time for XDG compliance
 sudo ln -sf ~/.config/pam_env.conf /etc/security/pam_env.conf
-sudo ln -sf ~/.config/X11/xinitrc /etc/security/custom_xinitrc.sh
+
+#Use xinitrc.d to make X11 XDG compliant
+sudo ln -sf ~/.config/X11/xinitrc /etc/X11/xinit/xinitrc.d/custom_xinitrc.sh
